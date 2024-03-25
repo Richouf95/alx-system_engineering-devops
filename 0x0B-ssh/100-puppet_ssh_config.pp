4833a7a2a96cbeef7b7587c5e2343e877a1e87a3
Richouf95 <wourichouf95@gmail.com>
2024-03-24 19:54:33 -0700

    client config with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,21 @@
+# config ssh client
+
+
+# replace the ssh private key entry
+file_line {'change private key source':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
+}
+
+
+# unautorize pwd using
+file_line {'unautaurize pwd using for connexion':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
+}
