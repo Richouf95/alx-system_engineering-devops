d19d6ca7eee482835a094bb32edcdceaf88c4044
Richouf95 <wourichouf95@gmail.com>
2024-05-16 20:19:49 +0000

    user_limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # User limit
 
-exec {
+exec {'increase-hard':
   command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
-exec {
+exec {'increase-soft':
   command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
